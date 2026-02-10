5711cceb9fb191b82532800927d2f40de0dbe28d
ESB19 <esballrounder@gmail.com>
2026-02-11 00:48:39 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("quran-audio-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
